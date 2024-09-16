389cf9529998399cea8bcc0d9df01afb72c4be38
anpha@DESKTOP-IT4F327 <anpha@DESKTOP-IT4F327.email@example.com>
2024-09-16 18:00:03 +0700

    first commit

--- ex1/App.js
+++ ex1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Xin chào sinh viên Phạm Quốc An - BIT220006!</Text>
       <StatusBar style="auto" />
     </View>
   );
